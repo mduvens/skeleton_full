eb206ed3fb2c58a5fced4b1d7eaaa12e39d1d9bb
manuel <mduvens@gmail.com>
2021-11-07 00:55:28 +0000

    m

--- server/index.js
+++ server/index.js
@@ -15,30 +15,108 @@ app.use(express.urlencoded({
 const log = (msg) => process.stdout.write(msg + '\n')
 
 const middleware = (req, res, next) => {
-    console.log("yoooooooooo");
+    // implement token verification or any other security measure for DB ops
     next()
 }
 /**
  * PRODUCT
  */
 app.route('/product')
-    .post(middleware, async function (req, res) {
+    .post( async function (req, res) {
         // post products
         const product = req.body;
-        await mongoose.saveProduct(product)
-        res.send("ok")
+        const result = await mongoose.saveProduct(product)
+        
+        res.json(result)
     })
     .get(async function (req, res) {
-        // retrieve products
+        // if query has name, retrive that product. if not, retrive all products
+        if('name' in req.query){
+            const result = await mongoose.getProduct(req.query.name);
+            
+            res.json(result)
+            return
+        }
+
         const result = await mongoose.getProducts();
-        console.log(result)
         res.json(result)
 
 
     })
-    .delete(function (req, res) {
+    .delete(async function (req, res) {
         // delete products
+       const data = req.body
+        if(data.name){
+            const result = await mongoose.deleteProduct(data.name);
+            res.json(result)
+        }
+      
+        else{
+            await mongoose.deleteAll()
+            res.json(222)
+        }
     })
+    .put(async function (req,res){
+        const data = req.body;
+        const filter = data.filter
+        const update = data.product
+
+        if(filter){
+            const result = await mongoose.updateProduct(filter, update);
+            res.json(result)
+        }
+        res.json()
+    })
+
+/**
+ * CATEGORY
+ */
+ app.route('/category')
+ .post( async function (req, res) {
+     // post categories
+     const category = req.body;
+     const result = await mongoose.saveCategory(category)
+     
+     res.json(result)
+ })
+ .get(async function (req, res) {
+     // if query has name, retrive that category. if not, retrieve all categories
+     if('name' in req.query){
+         const result = await mongoose.getCategory(req.query.name);
+         
+         res.json(result)
+         return
+     }
+
+     const result = await mongoose.getCategories();
+     res.json(result)
+
+
+ })
+ .delete(async function (req, res) {
+     // delete categorys
+    const data = req.body
+     if(data.name){
+         const result = await mongoose.deleteCategory(data.name);
+         res.json(result)
+     }
+   
+     else{
+         await mongoose.deleteAll()
+         res.json(222)
+     }
+ })
+ .put(async function (req,res){
+     const data = req.body;
+     const filter = data.filter
+     const update = data.category
+
+     if(filter){
+         const result = await mongoose.updateCategory(filter, update);
+         res.json(result)
+     }
+     res.json()
+ })
 
 
 // Static folder
--- server/mongoose/index.js
+++ server/mongoose/index.js
@@ -1,47 +1,96 @@
 // getting-started.js
 const mongoose = require('mongoose');
 
+const STATUS = {
+    SUCCESS: 222,
+    ERROR: 444
+}
+
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/test');
 }
 
-// Schemas
+// Category
+const categorySchema = new mongoose.Schema({
+    name: String
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+// Product
 const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
+    categories: Array
   });
 
-// Models
 const Product = mongoose.model('Product', productSchema);
 
-async function deleteProduct(name = null){
-    Product.deleteMany(name ? {name: name} : {}, (err, docs) => {
-        if(err){
-            console.log(err)
-        }
-        console.log("Deleted product " + name);
-    })
+async function deleteProduct(name){
+    if(name){
+        const result = await Product.deleteOne({name: name});
+    
+        return result.deletedCount > 0 ? STATUS.SUCCESS : STATUS.ERROR
+        
+    }
+    return STATUS.ERROR
 }
 
+async function deleteAll(){
+    try{
+        await Product.deleteMany({});
 
+        return STATUS.SUCCESS
+    }
+    catch(e){
+        return STATUS.ERROR
+    }
+    
+}
+
+async function updateProduct(filter, product){
+    delete product._id
+    
+    if(filter){
+        const before = await Product.findOneAndUpdate(filter,product);
+        
+        const after = await Product.findOne();
+        
+        if(before !== after){
+            return STATUS.SUCCESS
+        }
+
+        return STATUS.ERROR
+
+    }
+
+    return STATUS.ERROR
+}
 async function saveProduct(product){
-  if(product && 'name' in product && 'price' in product){
-    Product.find({name: product.name}, async (err, docs) => {
-        if(docs.length === 0){
+    console.log(product)
+    if(product && 'name' in product && 'price' in product){
+        const result = await Product.find({name: product.name}).exec()
+        console.log(result)
+        if(!result.length){
             await new Product({
                 name: product.name,
-                price: product.price
+                price: product.price,
+                categories: product.categories
                 
             }).save();
-            console.log('success')
+
+            return STATUS.SUCCESS
         }
-        else{
-            console.log("Error saving product " + product.name);
-        }
-    })    
     }
+
+    console.log("Error saving product " + product.name);
+    return STATUS.ERROR
+}
+async function getProduct(name){
+    const result = await Product.findOne({name:name})
+    return result
 }
 
 async function getProducts(){
@@ -49,8 +98,69 @@ async function getProducts(){
     return result
 }
 
+///////////// CATEGORY ////////////////////
+async function deleteCategory(name){
+    if(name){
+        const result = await Category.deleteOne({name: name});
+    
+        return result.deletedCount > 0 ? STATUS.SUCCESS : STATUS.ERROR
+        
+    }
+    return STATUS.ERROR
+}
+async function updateCategory(filter, category){
+    delete category._id
+    
+    if(filter){
+        const before = await Category.findOneAndUpdate(filter,category);
+        
+        const after = await Category.findOne();
+        
+        if(before !== after){
+            return STATUS.SUCCESS
+        }
+
+        return STATUS.ERROR
+
+    }
+    
+    return STATUS.ERROR
+}
+async function saveCategory(category){
+    if(category && 'name' in category){
+        const result = await Category.find({name: category.name}).exec()
+        
+        if(!result.length){
+            await new Category({
+                name: category.name
+                
+            }).save();
+            return STATUS.SUCCESS
+        }
+    }
+
+    console.log("Error saving category " + category.name);
+    return STATUS.ERROR
+}
+async function getCategory(name){
+    const result = await Category.findOne({name:name})
+    return result
+}
+
+async function getCategories(){
+    const result = await Category.find();
+    return result
+}
 module.exports = {
+    getProduct,
     getProducts,
     saveProduct,
-    deleteProduct
+    updateProduct,
+    deleteProduct,
+    getCategory,
+    getCategories,
+    saveCategory,
+    updateCategory,
+    deleteCategory,
+    deleteAll
 }
